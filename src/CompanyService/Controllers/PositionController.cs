9e30ed6769e2065fe1d94a7e98427773b8b55c95
Кирилл Пушкарев <part1s@mail.ru>
2020-09-14 18:25:57 +0300

    Added position controller

---
+++ src/CompanyService/Controllers/PositionController.cs
@@ -0,0 +1,43 @@
+﻿using LT.DigitalOffice.CompanyService.Business.Interfaces;
+using LT.DigitalOffice.CompanyService.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace LT.DigitalOffice.CompanyService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PositionController : ControllerBase
+    {
+        [HttpGet("getPositionById")]
+        public Position GetPositionById([FromServices] IGetPositionByIdCommand command, [FromQuery] Guid positionId)
+        {
+            return command.Execute(positionId);
+        }
+
+        [HttpGet("getPositionsList")]
+        public List<Position> GetPositionsList([FromServices] IGetPositionsListCommand command)
+        {
+            return command.Execute();
+        }
+
+        [HttpPost("addPosition")]
+        public Guid AddPosition([FromServices] IAddPositionCommand command, [FromBody] AddPositionRequest request)
+        {
+            return command.Execute(request);
+        }
+
+        [HttpDelete("disablePositionById")]
+        public void DisablePositionById([FromServices] IDisablePositionByIdCommand command, [FromQuery] Guid positionId)
+        {
+            command.Execute(positionId);
+        }
+
+        [HttpPost("editPosition")]
+        public bool EditPosition([FromServices] IEditPositionCommand command, [FromBody] EditPositionRequest request)
+        {
+            return command.Execute(request);
+        }
+    }
+}
