e38bd11833e5dea20ed0eac8f164690839a3e22e
lalag <lalagvanan@gmail.com>
2020-09-18 20:54:41 +0300

    Fixed healthcheck endpoint

--- src/CompanyService/Startup.cs
+++ src/CompanyService/Startup.cs
@@ -75,7 +75,7 @@ namespace LT.DigitalOffice.CompanyService
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseHealthChecks("/healthcheck");
+            app.UseHealthChecks("/api/healthcheck");
 
             app.UseExceptionHandler(tempApp => tempApp.Run(CustomExceptionHandler.HandleCustomException));
 
