464a6ebe7b07206fd9958c0e9923f57a698ce870
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2021-05-17 21:07:41 +0300

    Update version (#25)

--- src/CompanyService/Startup.cs
+++ src/CompanyService/Startup.cs
@@ -41,7 +41,7 @@ namespace LT.DigitalOffice.CompanyService
                 .GetSection(BaseServiceInfoConfig.SectionName)
                 .Get<BaseServiceInfoConfig>();
 
-            Version = "1.3.3";
+            Version = "1.3.4";
             Description = "CompanyService is an API that intended to work with positions and departments.";
             StartTime = DateTime.UtcNow;
             ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
