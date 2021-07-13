bd0e6c34e2c9bd5c6338f57491a81ae12ef57fdb
djino <malkinevgeniy437@gmail.com>
2021-07-13 04:47:17 +0300

    Fixed

--- src/CompanyService/Controllers/DepartmentController.cs
+++ src/CompanyService/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using LT.DigitalOffice.CompanyService.Business.Commands.Department.Interfaces;
 using LT.DigitalOffice.CompanyService.Models.Dto.Models;
 using LT.DigitalOffice.CompanyService.Models.Dto.Requests;
+using LT.DigitalOffice.CompanyService.Models.Dto.Requests.Department;
 using LT.DigitalOffice.CompanyService.Models.Dto.Requests.Filters;
 using LT.DigitalOffice.CompanyService.Models.Dto.Responses;
 using LT.DigitalOffice.Kernel.Enums;
