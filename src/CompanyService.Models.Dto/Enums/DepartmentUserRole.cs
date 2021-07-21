bcbde2f8b53199c9df8b525f1e7e646d9105cfad
BelehovEgor <belehov.egor2001@yandex.ru>
2021-07-21 12:50:52 +0300

    fix enum

--- src/CompanyService.Models.Dto/Enums/DepartmentUserRole.cs
+++ src/CompanyService.Models.Dto/Enums/DepartmentUserRole.cs
@@ -1,5 +1,9 @@
-﻿namespace LT.DigitalOffice.CompanyService.Models.Dto.Enums
+﻿using Newtonsoft.Json.Converters;
+using System.Text.Json.Serialization;
+
+namespace LT.DigitalOffice.CompanyService.Models.Dto.Enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum DepartmentUserRole
     {
         Employee,
