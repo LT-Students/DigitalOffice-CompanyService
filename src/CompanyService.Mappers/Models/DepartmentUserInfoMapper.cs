fcd43cf60beb993eeea675d59cb1c4916703d029
BelehovEgor <belehov.egor2001@yandex.ru>
2021-08-04 08:36:05 +0300

    fix

--- src/CompanyService.Mappers/Models/DepartmentUserInfoMapper.cs
+++ src/CompanyService.Mappers/Models/DepartmentUserInfoMapper.cs
@@ -3,30 +3,25 @@ using LT.DigitalOffice.CompanyService.Models.Db;
 using LT.DigitalOffice.CompanyService.Models.Dto.Models;
 using LT.DigitalOffice.Models.Broker.Models;
 using System;
-using LT.DigitalOffice.Kernel.AccessValidatorEngine.Interfaces;
 
 namespace LT.DigitalOffice.CompanyService.Mappers.Models
 {
     public class DepartmentUserInfoMapper : IDepartmentUserInfoMapper
     {
         private readonly IPositionInfoMapper _positionInfoMapper;
-        private readonly IAccessValidator _accessValidator;
         private readonly IImageInfoMapper _imageMapper;
 
         public DepartmentUserInfoMapper(
             IPositionInfoMapper positionInfoMapper,
-            IAccessValidator accessValidator,
             IImageInfoMapper imageMapper)
         {
             _positionInfoMapper = positionInfoMapper;
-            _accessValidator = accessValidator;
             _imageMapper = imageMapper;
         }
 
         public UserInfo Map(UserData userData, DbPositionUser dbPositionUser, ImageData image)
         {
-            //I don't understand this
-            if (userData == null || (dbPositionUser == null && !_accessValidator.IsAdmin(userData.Id)))
+            if (userData == null)
             {
                 throw new ArgumentNullException(nameof(userData));
             }
