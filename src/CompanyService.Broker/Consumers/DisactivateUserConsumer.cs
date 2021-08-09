73bd754e5bada3f73d35b7ef24ac0d1e6e9fd99b
BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
2021-08-09 18:02:28 +0300

    add consumer (#64)

---
+++ src/CompanyService.Broker/Consumers/DisactivateUserConsumer.cs
@@ -0,0 +1,29 @@
+﻿using LT.DigitalOffice.CompanyService.Data.Interfaces;
+using LT.DigitalOffice.Models.Broker.Common;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.CompanyService.Broker.Consumers
+{
+    public class DisactivateUserConsumer : IConsumer<IDisactivateUserRequest>
+    {
+        private readonly IDepartmentUserRepository _departmentUserRepository;
+        private readonly IPositionUserRepository _positionUserRepository;
+
+        public DisactivateUserConsumer(
+            IDepartmentUserRepository departmentUserRepository,
+            IPositionUserRepository positionUserRepository)
+        {
+            _departmentUserRepository = departmentUserRepository;
+            _positionUserRepository = positionUserRepository;
+        }
+
+        public Task Consume(ConsumeContext<IDisactivateUserRequest> context)
+        {
+            _departmentUserRepository.Remove(context.Message.UserId);
+            _positionUserRepository.Remove(context.Message.UserId);
+
+            return Task.FromResult(0);
+        }
+    }
+}
