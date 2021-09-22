2bec6b048cf61688a9668e2c06592603f83f1777
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-09-22 16:18:26 +0300

    rework get departmentUsers from IGetDepartmentUsersRequest (#78)
    * rework get departmentUsers from IGetDepartmentUsersRequest
    
    * fix
    
    * fix repo
    
    * add leftAt column
    
    * fix find method
    
    * fix
    
    * fix name
    
    * fix spases

--- src/CompanyService.Models.Db/DbDepartmentUser.cs
+++ src/CompanyService.Models.Db/DbDepartmentUser.cs
@@ -1,40 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
 
 namespace LT.DigitalOffice.CompanyService.Models.Db
 {
-    public class DbDepartmentUser
+  public class DbDepartmentUser
+  {
+    public const string TableName = "DepartmentsUsers";
+
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public Guid DepartmentId { get; set; }
+    public int Role { get; set; }
+    public bool IsActive { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public Guid? ModifiedBy { get; set; }
+    public DateTime? ModifiedAtUtc { get; set; }
+    public DateTime? LeftAtUts { get; set; }
+    public DbDepartment Department { get; set; }
+  }
+
+  public class DbDepartmentUserConfiguration : IEntityTypeConfiguration<DbDepartmentUser>
+  {
+    public void Configure(EntityTypeBuilder<DbDepartmentUser> builder)
     {
-        public const string TableName = "DepartmentsUsers";
+      builder
+        .ToTable(DbDepartmentUser.TableName);
 
-        public Guid Id { get; set; }
-        public Guid UserId { get; set; }
-        public Guid DepartmentId { get; set; }
-        public int Role { get; set; }
-        public bool IsActive { get; set; }
-        public Guid CreatedBy { get; set; }
-        public DateTime CreatedAtUtc { get; set; }
-        public Guid? ModifiedBy { get; set; }
-        public DateTime? ModifiedAtUtc { get; set; }
+      builder
+        .HasKey(u => u.Id);
 
-        public DbDepartment Department { get; set; }
+      builder
+        .HasOne(u => u.Department)
+        .WithMany(d => d.Users)
+        .HasForeignKey(u => u.DepartmentId);
     }
-
-    public class DbDepartmentUserConfiguration : IEntityTypeConfiguration<DbDepartmentUser>
-    {
-        public void Configure(EntityTypeBuilder<DbDepartmentUser> builder)
-        {
-            builder
-                .ToTable(DbDepartmentUser.TableName);
-
-            builder
-                .HasKey(u => u.Id);
-
-            builder
-                .HasOne(u => u.Department)
-                .WithMany(d => d.Users)
-                .HasForeignKey(u => u.DepartmentId);
-        }
-    }
+  }
 }
