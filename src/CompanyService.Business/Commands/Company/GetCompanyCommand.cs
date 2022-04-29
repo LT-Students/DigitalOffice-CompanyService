574eee1225d577bd745430884ffa959623a4d93d
Mariya <mariyasin@list.ru>
2022-04-29 11:36:28 +0300

    fix get company command

--- src/CompanyService.Business/Commands/Company/GetCompanyCommand.cs
+++ src/CompanyService.Business/Commands/Company/GetCompanyCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using LT.DigitalOffice.CompanyService.Broker.Requests.Interfaces;
 using LT.DigitalOffice.CompanyService.Business.Commands.Company.Interfaces;
@@ -9,6 +10,7 @@ using LT.DigitalOffice.CompanyService.Models.Db;
 using LT.DigitalOffice.CompanyService.Models.Dto.Requests.Company.Filters;
 using LT.DigitalOffice.CompanyService.Models.Dto.Responses;
 using LT.DigitalOffice.Kernel.Enums;
+using LT.DigitalOffice.Kernel.Helpers.Interfaces;
 using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.Models.Broker.Models.Office;
 
@@ -19,21 +21,31 @@ namespace LT.DigitalOffice.CompanyService.Business.Commands.Company
     private readonly ICompanyRepository _repository;
     private readonly ICompanyResponseMapper _mapper;
     private readonly IOfficeService _officeService;
+    private readonly IResponseCreator _reponseCreator;
 
     public GetCompanyCommand(
       ICompanyRepository repository,
       ICompanyResponseMapper mapper,
-      IOfficeService officeService)
+      IOfficeService officeService,
+      IResponseCreator reponseCreator)
     {
       _repository = repository;
       _mapper = mapper;
       _officeService = officeService;
+      _reponseCreator = reponseCreator;
     }
 
     public async Task<OperationResultResponse<CompanyResponse>> ExecuteAsync(GetCompanyFilter filter)
     {
-      List<string> errors = new();
+      
       DbCompany company = await _repository.GetAsync();
+
+      if (company is null)
+      {
+        return _reponseCreator.CreateFailureResponse<CompanyResponse>(HttpStatusCode.NotFound);
+      }
+
+      List<string> errors = new();
 
       List<OfficeData> offices = filter.IncludeOffices
         ? await _officeService.GetOfficesAsync(errors)
